13aac08f6da5084a3690f3cfe03d7b2781682eea
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-05-05 09:49:48 +0500

    castling

--- padding_table.c
+++ padding_table.c
@@ -2,17 +2,26 @@
 
 void shifting(char *file_name) {
     struct line_table *lt = read_struct(file_name);
-    
+
     write_struct(file_name, lt);
     printf("\nShifting done\n");
 }
 
 void castling(char *file_name) {
     struct line_table *lt = read_struct(file_name);
-    int i = 0;
-    while(lt[i].line_key != 0) {
-        printf("%d - %s\n", lt[i].line_key, lt[i].line_data);
-        i++;
+    int tmp_key;
+    char *tmp_data;
+    int length_table = 0;
+    while(lt[length_table].line_key != 0) {
+        length_table++;
+    }
+    for(int i = 0; i < (int)(length_table / 2); i++) {
+        tmp_key = lt[i].line_key;
+        tmp_data = lt[i].line_data;
+        lt[i].line_key = lt[length_table - i - 1].line_key;
+        lt[i].line_data = lt[length_table - i - 1].line_data;
+        lt[length_table - i - 1].line_key = tmp_key;
+        lt[length_table - i - 1].line_data = tmp_data;
     }
     write_struct(file_name, lt);
     printf("\nCastling done\n");
